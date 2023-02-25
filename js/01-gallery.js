7e092897cd034fb19dbdc3ce91693cbbf9fc5022
Inna Zorina <innazorina0101@gmail.com>
2023-02-25 14:03:09 +0200

    робить завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -13,3 +13,49 @@ console.log(galleryItems);
 //     />
 //   </a>
 // </div> */}
+
+const gallery = document.querySelector(".gallery");
+const markup = creatGalleryMarkup(galleryItems);
+
+gallery.innerHTML = markup;
+
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>`;
+    })
+    .join();
+
+  console.log(markup);
+}
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+        
+	}
+   
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
